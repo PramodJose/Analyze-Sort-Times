3f3f387cd7bd90db15e53b604d6bdfdce4fe3177
PramodJose <pramod.gj17@gmail.com>
2018-09-13 23:25:32 +0530

    Fixed a side effect bug

--- InsSort.c
+++ InsSort.c
@@ -10,8 +10,10 @@ void insSort(int *arr, int n)
 			printf("\t\t\t\t\r%lf%% completed", (i * 100.0)/n);
 			fflush(NULL);
 		}
+
 		p = i - 1;
 		t = arr[i];
+
 		while(p >= 0 && t < arr[p])
 		{
 			arr[p + 1] = arr[p];
